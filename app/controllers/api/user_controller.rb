f6d378aa27d7b958cb4321b6536ff6b7f876ee56
MarioTriMar <MarioJose.Trivino@alu.uclm.es>
2023-04-20 18:59:06 +0200

    funcionalidad admin

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,14 +1,17 @@
 class Api::UserController < ApplicationController
     def addUser
-        
         if(params[:password] == params[:password2])
                 if params[:password].match?(/^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/)
-                    user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
-                    user.password_digest = BCrypt::Password.create(user.password_digest)
-                    if user.save()
-                        render json:user, status: :ok
+                    if params[:phone].match?(/\A([6-9]\d{8})\z/)
+                        user = User.new(username: params[:username], email: params[:email], phone: params[:phone], type: params[:type], password_digest: params[:password])
+                        user.password_digest = BCrypt::Password.create(user.password_digest)
+                        if user.save()
+                            render json:user, status: :ok
+                        else
+                            render json: {message: "User not added"}, status: :unprocessable_entity
+                        end
                     else
-                        render json: {message: "User not added"}, status: :unprocessable_entity
+                        render json: {message: "The phone number must be between 600 00 00 00 and 999 99 99 99"}, status: :unprocessable_entity
                     end
                 else
                     render json: {message: "The password must be at least 8 characters and at least one of them must be a digit"}, status: :unprocessable_entity
@@ -28,6 +31,28 @@ class Api::UserController < ApplicationController
             end
         else
             render json: {msg: 'User does not exist'}, status: :unprocessable_entity
+        end
+    end
+    def getAllUsers
+        user= User.where(type:'user')
+        if user
+            render json:user, status: :ok
+        else
+            render json: {msg: 'Note not found'}, status: :unprocessable_entity
+        end
+    end
+    def updateUser
+        
+        if params[:phone].match?(/\A([6-9]\d{8})\z/)
+            user = User.find(params[:_id])
+            if user.username!=params[:username]
+                Note.where(idUser: user.username).update_all(idUser: params[:username])
+            end
+            puts params[:username]
+            User.find(params[:_id]).update(username: params[:username], email: params[:email], phone: params[:phone], type: params[:type])
+            
+        else
+            render json: {message: "The phone number must be between 600 00 00 00 and 999 99 99 99"}, status: :unprocessable_entity
         end
     end
 end
